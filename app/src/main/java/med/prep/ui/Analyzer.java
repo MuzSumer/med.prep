b8ab65fc34cfa515a17d73de407614184e95ba70
Maria Sumer <pinimento@gmail.com>
2025-09-23 16:39:28 +0200

    V 1.5

--- app/src/main/java/med/prep/ui/Analyzer.java
+++ app/src/main/java/med/prep/ui/Analyzer.java
@@ -1,6 +1,7 @@
 package med.prep.ui;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,7 +27,7 @@ public class Analyzer extends Fragment {
     public DiagramExpose expo() { return expo; }
 
     int emergency = 11;
-    int order = 37;
+    int order = 33;
 
     String fullname;
     String birthdate;
@@ -71,7 +72,10 @@ public class Analyzer extends Fragment {
         TextView best = view.findViewById(R.id.result_best);
         TextView worst = view.findViewById(R.id.result_worst);
         TextView average = view.findViewById(R.id.result_average);
+        TextView load = view.findViewById(R.id.result_load);
 
+        TextView dload = view.findViewById(R.id.d_load);
+        dload.setText(order + " Tage Vorrat");
 
         // *** analysis
 
@@ -83,14 +87,10 @@ public class Analyzer extends Fragment {
 
             average_days = (average_days + restdays)/2;
 
-            if (restdays > best_days) {
-                best_days = restdays;
-            }
+            if (restdays > best_days) { best_days = restdays; }
 
+            if (restdays < worst_days) { worst_days = restdays; }
 
-            if (restdays < worst_days) {
-                worst_days = restdays;
-            }
 
         }//next model
 
@@ -103,16 +103,26 @@ public class Analyzer extends Fragment {
 
 
 
+        if (worst_days > order) {
+            load.setTextColor(Color.BLACK);
+            load.setText("100 %");
+        }
+        else {
+            load.setTextColor(Color.RED);
+            load.setText(worst_days + "/" + order);
+
+            double l = (double) (100 * worst_days/order);
+            //l += 0.5;
+            String v = Double.toString(l);
+            int p = v.indexOf(".");
 
 
+            load.setText(v.substring(0, p) + " %");
+        }
 
 
         //write analytical text
         analyticalText(view);
-
-
-
-
 
 
         return view;
@@ -122,17 +132,20 @@ public class Analyzer extends Fragment {
 
         TextView tv = view.findViewById(R.id.result_output);
 
+        tv.setTextColor(Color.BLUE);
+
         if (worst_days < emergency) {
 
             /* Nachschub umgehend erforderlich */
             tv.setText(getString(R.string.analyze_emergency));
+            tv.setTextColor(Color.RED);
 
 
 
 
         } else if (worst_days < order) {
 
-            /* Nachschub empfohlen */
+            /* Nachschub baldmöglich empfohlen */
             tv.setText(getString(R.string.analyze_stockup));
 
 
